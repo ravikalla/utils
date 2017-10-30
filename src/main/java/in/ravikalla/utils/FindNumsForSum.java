648d3fc876075238c6fa70a1511058fae097bcc1
Ravi Kalla <ravi2523096@gmail.com>
2017-10-30 17:58:07 -0400

    Included test cases

--- src/main/java/in/ravikalla/utils/FindNumsForSum.java
+++ src/main/java/in/ravikalla/utils/FindNumsForSum.java
@@ -4,14 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FindNumsForSum {
-	public static void main( String[] args ) {
-		int[] arrNum = {1,2,3,4,5,6,7,8,9,0};
-		int intSum = 10;
+	private int[] arrNum;
+	private int intExpectedSum;
 
-		findNumsForSum(arrNum, intSum, new ArrayList<Integer>(), 0);
+	List<List<Integer>> lstResult;
+
+	public FindNumsForSum(int[] arrNum, int intExpectedSum) {
+		this.arrNum = arrNum;
+		this.intExpectedSum = intExpectedSum;
+		lstResult = new ArrayList<>();
 	}
 
-	private static void findNumsForSum(int[] arrNum, int intExpectedSum, List<Integer> lstNum, int intCurrPos) {
+	public List<List<Integer>> findNumsForSum() {
+		findNumsForSum(new ArrayList<Integer>(), 0);
+		return lstResult;
+	}
+
+	private void findNumsForSum(List<Integer> lstNum, int intCurrPos) {
 
 		int intParentValueSize = sumOfNumsInList(lstNum);
 		int intSumCurrentLevel;
@@ -19,18 +28,20 @@ public class FindNumsForSum {
 		for (int intCurrPosLocal = intCurrPos; intCurrPosLocal < arrNum.length; intCurrPosLocal++) {
 			intSumCurrentLevel = intParentValueSize + arrNum[intCurrPosLocal];
 
-			if (intSumCurrentLevel == intExpectedSum)
-				printResult(lstNum, arrNum[intCurrPosLocal]);
+			if (intSumCurrentLevel == intExpectedSum) {
+//				printResult(lstNum, arrNum[intCurrPosLocal]);
+				saveResult(lstNum, arrNum[intCurrPosLocal]);
+			}
 			else if ((intSumCurrentLevel < intExpectedSum)
 					&& (intCurrPosLocal + 1 < arrNum.length)) {
 				lstNum.add(arrNum[intCurrPosLocal]);
-				findNumsForSum(arrNum, intExpectedSum, lstNum, intCurrPosLocal + 1);
+				findNumsForSum(lstNum, intCurrPosLocal + 1);
 				lstNum.remove(lstNum.size() - 1);
 			}			
 		}
 	}
 
-	private static int sumOfNumsInList(List<Integer> lstNum) {
+	private int sumOfNumsInList(List<Integer> lstNum) {
 		int intSum = 0;
 		if (null != lstNum)
 			for (Integer i : lstNum)
@@ -38,12 +49,30 @@ public class FindNumsForSum {
 		return intSum;
 	}
 
-	private static void printResult(List<Integer> lstNum, int intLastValue) {
-		System.out.print("Numbers with the expected sum ");
-		if (null != lstNum) {
-//			lstNum.forEach(System.out::print);
-			lstNum.forEach(i -> {System.out.print(" : " + i);});
-		}
-		System.out.println(" : " + intLastValue);
+//	private void printResult(List<Integer> lstNum, int intLastValue) {
+//		System.out.print("Numbers with the expected sum ");
+//		if (null != lstNum) {
+////			lstNum.forEach(System.out::print);
+//			lstNum.forEach(i -> {System.out.print(" : " + i);});
+//		}
+//		System.out.println(" : " + intLastValue);
+//	}
+
+	private void saveResult(List<Integer> lstNum, int intLastValue) {
+		List<Integer> lstResultTemp = new ArrayList<>(lstNum);
+		lstResultTemp.add(intLastValue);
+
+		lstResult.add(lstResultTemp);
+	}
+
+	public static void main(String[] args) {
+		int[] arrNum = {1,2,3,4,5,6,7,8,9,0};
+		int intSum = 10;
+
+		FindNumsForSum objFindNumsForSum = new FindNumsForSum(arrNum, intSum);
+		List<List<Integer>> lstResults = objFindNumsForSum.findNumsForSum();
+
+		System.out.println("Number sets that has the sum = " + intSum);
+		lstResults.forEach(l -> {l.forEach(j -> {System.out.print(": " + j);});System.out.println("");});
 	}
 }
--- src/test/java/in/ravikalla/utils/FindNumsForSumTest.java
+++ src/test/java/in/ravikalla/utils/FindNumsForSumTest.java
@@ -1,10 +1,14 @@
 package in.ravikalla.utils;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Unit test for simple App.
@@ -19,6 +23,49 @@ public class FindNumsForSumTest extends TestCase {
 	}
 
 	public void testFindNumsForSum() {
-		assertTrue(true);
+		List<Integer> lstExpected0 = Arrays.asList(1, 2, 3, 4);
+		List<Integer> lstExpected1 = Arrays.asList(1, 2, 7);
+		List<Integer> lstExpected2 = Arrays.asList(1, 3, 6);
+		List<Integer> lstExpected3 = Arrays.asList(1, 4, 5);
+		List<Integer> lstExpected4 = Arrays.asList(1, 9);
+		List<Integer> lstExpected5 = Arrays.asList(2, 3, 5);
+		List<Integer> lstExpected6 = Arrays.asList(2, 8);
+		List<Integer> lstExpected7 = Arrays.asList(3, 7);
+		List<Integer> lstExpected8 = Arrays.asList(4, 6);
+
+		int[] arrNum = {1,2,3,4,5,6,7,8,9,0};
+		int intSum = 10;
+
+		FindNumsForSum objFindNumsForSum = new FindNumsForSum(arrNum, intSum);
+		List<List<Integer>> lstResults = objFindNumsForSum.findNumsForSum();
+
+		Assert.assertEquals("Number of results should be as expected", 9, lstResults.size());
+		assertThat(lstResults.get(0), is(lstExpected0));
+		assertThat(lstResults.get(1), is(lstExpected1));
+		assertThat(lstResults.get(2), is(lstExpected2));
+		assertThat(lstResults.get(3), is(lstExpected3));
+		assertThat(lstResults.get(4), is(lstExpected4));
+		assertThat(lstResults.get(5), is(lstExpected5));
+		assertThat(lstResults.get(6), is(lstExpected6));
+		assertThat(lstResults.get(7), is(lstExpected7));
+		assertThat(lstResults.get(8), is(lstExpected8));
+	}
+
+	public void testFindNumsForSum1() {
+		List<Integer> lstExpected0 = Arrays.asList(1, 4);
+		List<Integer> lstExpected1 = Arrays.asList(2, 3);
+		List<Integer> lstExpected2 = Arrays.asList(5);
+
+		int[] arrNum = {1,2,3,4,5,6,7,8,9,0};
+		int intSum = 5;
+
+		FindNumsForSum objFindNumsForSum = new FindNumsForSum(arrNum, intSum);
+		List<List<Integer>> lstResults = objFindNumsForSum.findNumsForSum();
+
+		Assert.assertEquals("Number of results should be as expected", 3, lstResults.size());
+		assertThat(lstResults.get(0), is(lstExpected0));
+		assertThat(lstResults.get(1), is(lstExpected1));
+		assertThat(lstResults.get(2), is(lstExpected2));
+//		lstResults.forEach(l -> {l.forEach(j -> {System.out.print(": " + j);});System.out.println("");});
 	}
 }
