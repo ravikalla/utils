1b0cd524c84897aef05b51d5f683b3ca7c73e042
Ravi Kalla <ravi2523096@gmail.com>
2017-10-30 13:19:46 -0400

    Util project setup

---
+++ src/main/java/in/ravikalla/utils/App.java
@@ -0,0 +1,13 @@
+package in.ravikalla.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
